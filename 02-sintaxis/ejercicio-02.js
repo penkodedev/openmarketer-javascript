c2c687c941291f378df102b1c708a88af4cb4dc4
Penkode <85491812+penkodedev@users.noreply.github.com>
2022-12-22 17:58:42 +0100

    Add files via upload

---
+++ 02-sintaxis/ejercicio-02.js
@@ -0,0 +1,17 @@
+
+const paulo = {
+    Nombre: "Paulo Ricardo Ramalho e Silva",
+    Edad: 51,
+    Desarrolador: true,
+}
+
+const fechaCumple = new Date("march 28 1971")
+
+const libro = {
+    Titulo: "The Fall of the House of Usher",
+    Autor: " Edgar Allan Poe",
+    Fecha: 1839,
+    URL: "https://es.wikipedia.org/wiki/La_ca%C3%ADda_de_la_Casa_Usher"
+}
+    
+console.log(paulo, libro, fechaCumple)
