dea3b4f1accf6b3314a656508fb869445aebf0ad
Penkode <85491812+penkodedev@users.noreply.github.com>
2023-02-16 14:03:14 +0100

    Add files via upload

---
+++ 05-numbers/ejercicio-numbers.js
@@ -0,0 +1,14 @@
+let alturaCent = 177;
+let alturaMetros = 1.77;
+let peso = 78.1;
+
+let alturaMetrosArriba = Math.ceil(alturaMetros * 10) / 10;
+let pesoBajo = Math.floor(peso - 0.1) / 10;
+
+
+
+console.log(alturaMetrosArriba);
+console.log(pesoBajo);
+
+let maxValor = Number.MAX_VALUE;
+console.log(maxValor)
